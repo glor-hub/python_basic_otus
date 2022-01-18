2dd98d9ad3b6379ece9285d2b0d4d66d1b081fba
Inna Sorina <sorina-inna@yandex.ru>
2022-01-18 20:17:42 +0300

    Add homework_02.

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,16 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+class Car(Vehicle):
+    engine = (0,0)
+
+    def set_engine(self, engine):
+       self.engine=engine
+
+if __name__ == '__main__':
+    r=Car(4,5,6)
+    r.set_engine(Engine(2,3))
+    print(r.__dict__.values())
