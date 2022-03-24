dced22577b2532656ee02631960e658e1029b343
Inna Sorina <sorina-inna@yandex.ru>
2022-03-24 23:16:30 +0300

    Fix jsonplaceholder_requests.py.

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -11,7 +11,7 @@ from homework_04.models import (
     Post,
 )
 
-from typing import List
+from typing import List, Any
 
 
 NUM_USERS = 10
@@ -19,26 +19,31 @@ NUM_USERS = 10
 USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
-async def fetch_json(session: ClientSession, url: str) -> dict:
+async def fetch_json(session: ClientSession, url: str) -> Any:
     async with session.get(url) as response:  # : ClientResponse
         if response.status == HTTPStatus.OK:
             response_json = await response.json()
             return response_json
+        else:
+            return None
 
 
 async def fetch_user_by_user_id(userId: int) -> User:
     async with ClientSession() as session:
         user_json = await fetch_json(session, USERS_DATA_URL + "/" + str(userId))
-    id = user_json.get("id", 1)
-    name = user_json.get("name", "")
-    username = user_json.get("username", "")
-    email = user_json.get("email", "")
-    user = User(
-        id=id,
-        name=name,
-        username=username,
-        email=email
-    )
+    if user_json is not None:
+        id = user_json.get("id", 1)
+        name = user_json.get("name", "")
+        username = user_json.get("username", "")
+        email = user_json.get("email", "")
+        user = User(
+            id=id,
+            name=name,
+            username=username,
+            email=email
+        )
+    else:
+        user=None
     return user
 
 
@@ -46,18 +51,19 @@ async def fetch_posts_for_user(userId: int) -> List:
     posts_for_user = []
     async with ClientSession() as session:
         posts_json = list(await fetch_json(session, POSTS_DATA_URL + "?" + "userId=" + str(userId)))
-    for post_json in posts_json:
-        id = post_json.get("id", 0)
-        user_id = post_json.get("userId", 0)
-        title = post_json.get("title", "")
-        body = post_json.get("body", "")
-        post = Post(
-            id=id,
-            user_id=user_id,
-            title=title,
-            body=body
-        )
-        posts_for_user.append(post)
+    if posts_json is not None:
+        for post_json in posts_json:
+            id = post_json.get("id", 0)
+            user_id = post_json.get("userId", 0)
+            title = post_json.get("title", "")
+            body = post_json.get("body", "")
+            post = Post(
+                id=id,
+                user_id=user_id,
+                title=title,
+                body=body
+            )
+            posts_for_user.append(post)
     return posts_for_user
 
 async def fetch_users_data():
