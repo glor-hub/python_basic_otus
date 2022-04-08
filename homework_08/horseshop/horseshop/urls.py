7c3e324e2194962dabdc3afc9af49985dc6c6f86
glor-hub <56581877+glor-hub@users.noreply.github.com>
2022-04-08 10:33:33 +0300

    Update urls.py

--- homework_08/horseshop/horseshop/urls.py
+++ homework_08/horseshop/horseshop/urls.py
@@ -1,7 +1,7 @@
 """horseshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from horseshop.settings import DEBUG
+import product.views as product
 
 urlpatterns = [
+    path('', product.index,  name='home'),
+    path('product/detail/<int:pk>/', product.ProductDetailView.as_view(), name='product_detail'),
+    path('product/list/', product.ProductsListView.as_view(), name='product_list'),
+
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if DEBUG:
-    urlpatterns += path('__debug__/', include('debug_toolbar.urls')),
+    urlpatterns += path('__debug__/', include('debug_toolbar.urls')), 
+    
