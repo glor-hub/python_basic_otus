59918d360bcc12fe2614183e6d3a7b16ca48590f
Inna Sorina <sorina-inna@yandex.ru>
2022-04-17 23:52:11 +0300

    Split fill_db.py into files: fill_db1.py and fill_db2.py. Add .toml file.

--- homework_09/horseshop/product/management/commands/fill_db1.py
+++ homework_09/horseshop/product/management/commands/fill_db1.py
@@ -36,29 +36,3 @@ class Command(BaseCommand):
                                                email=contacts[company]["email"]
                                                )
 
-    goods = {'saddle pad "COTTON" DR': {'price': 500,
-                                        'count': 29,
-                                        'manufacturer': Manufacturer(id=1)},
-             'gloves "TRYON"': {'price': 150,
-                                'count': 8,
-                                'manufacturer': Manufacturer(id=2)},
-             'breeches "Performans"': {'price': 378,
-                                       'count': 17,
-                                       'manufacturer': Manufacturer(id=3)},
-             'helmet "EQUESTRO FRAME"': {'price': 450,
-                                         'count': 3,
-                                         'manufacturer': Manufacturer(id=4)}
-             }
-    products = [
-        'saddle pad "COTTON" DR',
-        'gloves "TRYON"',
-        'breeches "Performans"',
-        'helmet "EQUESTRO FRAME"'
-    ]
-    for product in products:
-        Product.objects.get_or_create(name=product,
-                                      curr_price=goods[product]["price"],
-                                      curr_count=goods[product]["count"],
-                                      manufacturer=goods[product]["manufacturer"]
-                                      )
-
